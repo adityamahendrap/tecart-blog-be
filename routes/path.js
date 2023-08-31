db974f776facc1820f0d3784f2b51a118ccb56be
adityamahendrap <ptadityamahendrap@gmail.com>
2023-08-31 21:53:36 +0800

    adjust route

--- routes/path.js
+++ routes/path.js
@@ -1,72 +1,284 @@
 // sufix SELF mean you dont need to define userId. JWT already handle it
 // don't forget to use prefix /api for the path
 export default {
-  LOGIN: "/auth/login",
-  REGISTER: "/auth/register",
-  VERIFY_TOKEN: "/auth/verify-token",
-  VERIFY_EMAIL: "/auth/verify-email/:id",
-  RESEND_EMAIL_VERIFICATION: "/auth/send-email-verification",
-  SEND_EMAIL_RESET_PASSWORD: "/auth/send-email-reset-password",
-  RESET_PASSWORD: "/auth/reset-password",
-  CHANGE_PASSWORD: "/auth/change-password", 
-  LOGOUT: "/auth/logout", // ??
-  // GITHUB_OAUTH: "/oauth/github",
-  // GOOGLE_OAUTH: "/oauth/google",
-  GITHUB_OAUTH: "/oauth/github",
-  GOOGLE_OAUTH: "/oauth/google",
-  FACEBOOK_OAUTH: "/oauth/facebook",
-
-  ME: "/me",
-  GET_USERS: "/users",
-  GET_USER: "/users/:id",
-  UPDATE_USER_SELF: "/users/me",
-  DELETE_ACCOUNT_SELF: "/users/me",
-  SET_NEW_USER_PREFERENCE_SELF: "/users/me/preference/new",
-
-  GET_POSTS: "/posts",
-  GET_POST: "/posts/:id",
-  GET_RELATED_POSTS: "/posts/:id/related",
-  CREATE_POST_SELF: "/posts",
-  UPDATE_POST: "/posts/:id",
-  DELETE_POST: "/posts/:id",
-  INCREMENT_VIEWS: "/posts/:id/views-plus-plus",
-
-  GET_RANDOM_POSTS_FOR_FEEDS: "/feeds/random/:total",
-  GET_RELEVANT_POSTS_FOR_FEEDS: "/feeds/relevant",
-  GET_LATEST_POSTS_FOR_FEEDS: "/feeds/latest",
-  GET_TOP_POSTS_FOR_FEEDS: "/feeds/top",
-
-  GET_CATEGORIES: "/categories",
-  GET_POPULAR_CATEGORIES: "/categories/popular",
-  GET_TAGS: "/tags",
-
-  GET_LIKES_COUNT_IN_POST: "/likes/:postId",
-  GET_LIKED_POSTS_BY_USER_SELF: "/likes",
-  LIKE_POST_SELF: "/likes/:postId",
-  UNLIKE_POST: "/likes/:id",
-
-  GET_COMMENTS_IN_POSTS: "/comments/:postId",
-  GET_ROOT_COMMENTS_IN_POSTS: "/comments/:postId", // ??
-  GET_REPLY_COMMENTS_IN_POSTS: "/comments/:commentId", // ??
-  CREATE_COMMENT_SELF: "/comments/:postId",
-  UPDATE_COMMENT: "/comments/:id",
-  DELETE_COMMENT: "/comments/:id",
-
-  COUNT_FOLLOWS: "/follows/:userId/total",
-  GET_FOLLOWERS: "/follows/followers/:userId",
-  GET_FOLLOWINGS: "/follows/followings/:userId",
-  FOLLOW_USER_SELF: "/follows/:targetId",
-  UNFOLLOW_USER_SELF: "/follows/:id",
-
-  GET_SHARED_POSTS: "/shares/:userId",
-  SHARE_POST: "/shares",
-  UPDATE_SHARED_POST: "/shares/:id",
-  DELETE_SHARED_POST: "/shares/:id",
-
-  GET_NOTIFICATIONS_SELF: "/notifications",
-  CREATE_NOTIFICATION: "/notifications/:userId", // ??
-
-  GET_SUBSCRIPTION: "/subscriptions/:targetId",
-  CREATE_SUBSCRIPTION_SELF: "/subscriptions/:targetId",
-  DELETE_SUBSCRIPTION: "/subscriptions/:id",
+  LOGIN: {
+    method: "post",
+    route: "/auth/login",
+    description: ""
+  },
+  REGISTER: {
+    method: "post",
+    route: "/auth/register",
+    description: ""
+  },
+  VERIFY_TOKEN: {
+    method: "post",
+    route: "/auth/verify-token",
+    description: ""
+  },
+  VERIFY_EMAIL: {
+    method: "get",
+    route: "/auth/verify-email/:id",
+    description: ""
+  },
+  RESEND_EMAIL_VERIFICATION: {
+    method: "get",
+    route: "/auth/send-email-verification",
+    description: ""
+  },
+  SEND_EMAIL_RESET_PASSWORD: {
+    method: "get",
+    route: "/auth/send-email-reset-password",
+    description: ""
+  },
+  RESET_PASSWORD: {
+    method: "put",
+    route: "/auth/reset-password",
+    description: ""
+  },
+  CHANGE_PASSWORD: {
+    method: "put",
+    route: "/auth/change-password",
+    description: ""
+  }, 
+  LOGOUT: { // ??
+    method: "get",
+    route: "/auth/logout",
+    description: ""
+  },
+  GITHUB_OAUTH: {
+    method: "get",
+    route: "/oauth/github",
+    description: ""
+  },
+  GOOGLE_OAUTH: {
+    method: "get",
+    route: "/oauth/google",
+    description: ""
+  },
+  FACEBOOK_OAUTH: {
+    method: "get",
+    route: "/oauth/facebook",
+    description: ""
+  },
+  ME: {
+    method: "get",
+    route: "/me",
+    description: ""
+  },
+  GET_USERS: {
+    method: "get",
+    route: "/users",
+    description: ""
+  },
+  GET_USER: {
+    method: "get",
+    route: "/users/:id",
+    description: ""
+  },
+  UPDATE_USER_SELF: {
+    method: "put",
+    route: "/users/me",
+    description: ""
+  },
+  DELETE_ACCOUNT_SELF: {
+    method: "delete",
+    route: "/users/me",
+    description: ""
+  },
+  SET_NEW_USER_PREFERENCE_SELF: {
+    method: "put",
+    route: "/users/me/preference/new",
+    description: ""
+  },
+  GET_POSTS: {
+    method: "get",
+    route: "/posts",
+    description: ""
+  },
+  GET_POST: {
+    method: "get",
+    route: "/posts/:id",
+    description: ""
+  },
+  GET_RELATED_POSTS: {
+    method: "get",
+    route: "/posts/:id/related",
+    description: ""
+  },
+  CREATE_POST_SELF: {
+    method: "post",
+    route: "/posts",
+    description: ""
+  },
+  UPDATE_POST: {
+    method: "put",
+    route: "/posts/:id",
+    description: ""
+  },
+  DELETE_POST: {
+    method: "delete",
+    route: "/posts/:id",
+    description: ""
+  },
+  INCREMENT_VIEWS: {
+    method: "put",
+    route: "/posts/:id/views-plus-plus",
+    description: ""
+  },
+  GET_RANDOM_POSTS_FOR_FEEDS: {
+    method: "get",
+    route: "/feeds/random/:total",
+    description: ""
+  },
+  GET_RELEVANT_POSTS_FOR_FEEDS: {
+    method: "get",
+    route: "/feeds/relevant",
+    description: ""
+  },
+  GET_LATEST_POSTS_FOR_FEEDS: {
+    method: "get",
+    route: "/feeds/latest",
+    description: ""
+  },
+  GET_TOP_POSTS_FOR_FEEDS: {
+    method: "get",
+    route: "/feeds/top",
+    description: ""
+  },
+  GET_CATEGORIES: {
+    method: "get",
+    route: "/categories",
+    description: ""
+  },
+  GET_POPULAR_CATEGORIES: {
+    method: "get",
+    route: "/categories/popular",
+    description: ""
+  },
+  GET_TAGS: {
+    method: "get",
+    route: "/tags",
+    description: ""
+  },
+  GET_LIKES_COUNT_IN_POST: {
+    method: "get",
+    route: "/likes/:postId",
+    description: ""
+  },
+  GET_LIKED_POSTS_BY_USER_SELF: {
+    method: "get",
+    route: "/likes",
+    description: ""
+  },
+  LIKE_POST_SELF: {
+    method: "post",
+    route: "/likes/:postId",
+    description: ""
+  },
+  UNLIKE_POST: {
+    method: "delete",
+    route: "/likes/:id",
+    description: ""
+  },
+  GET_COMMENTS_IN_POSTS: {
+    method: "get",
+    route: "/comments/:postId",
+    description: ""
+  },
+  GET_ROOT_COMMENTS_IN_POSTS: {
+    method: "get",
+    route: "/comments/:postId",
+    description: ""
+  },
+  GET_REPLY_COMMENTS_IN_POSTS: { // ??
+    method: "get",
+    route: "/comments/:commentId",
+    description: ""
+  },
+  CREATE_COMMENT_SELF: {
+    method: "post",
+    route: "/comments/:postId",
+    description: ""
+  },
+  UPDATE_COMMENT: {
+    method: "put",
+    route: "/comments/:id",
+    description: ""
+  },
+  DELETE_COMMENT: {
+    method: "delete",
+    route: "/comments/:id",
+    description: ""
+  },
+  COUNT_FOLLOWS: {
+    method: "get",
+    route: "/follows/:userId/total",
+    description: ""
+  },
+  GET_FOLLOWERS: {
+    method: "get",
+    route: "/follows/followers/:userId",
+    description: ""
+  },
+  GET_FOLLOWINGS: {
+    method: "get",
+    route: "/follows/followings/:userId",
+    description: ""
+  },
+  FOLLOW_USER_SELF: {
+    method: "post",
+    route: "/follows/:targetId",
+    description: ""
+  },
+  UNFOLLOW_USER_SELF: {
+    method: "delete",
+    route: "/follows/:id",
+    description: ""
+  },
+  GET_SHARED_POSTS: {
+    method: "get",
+    route: "/shares/:userId",
+    description: ""
+  },
+  SHARE_POST: {
+    method: "post",
+    route: "/shares",
+    description: ""
+  },
+  UPDATE_SHARED_POST: {
+    method: "put",
+    route: "/shares/:id",
+    description: ""
+  },
+  DELETE_SHARED_POST: {
+    method: "delete",
+    route: "/shares/:id",
+    description: ""
+  },
+  GET_NOTIFICATIONS_SELF: {
+    method: "get",
+    route: "/notifications",
+    description: ""
+  },
+  CREATE_NOTIFICATION: { // ??
+    method: "post",
+    route: "/notifications/:userId",
+    description: ""
+  },
+  GET_SUBSCRIPTION: {
+    method: "get",
+    route: "/subscriptions/:targetId",
+    description: ""
+  },
+  CREATE_SUBSCRIPTION_SELF: {
+    method: "post",
+    route: "/subscriptions/:targetId",
+    description: ""
+  },
+  DELETE_SUBSCRIPTION: {
+    method: "delete",
+    route: "/subscriptions/:id",
+    description: ""
+  },
 };
--- routes/routes.js
+++ routes/routes.js
@@ -16,68 +16,109 @@ import express, { Router } from "express";
 const app = express();
 const router = Router();
 
-router.post(path.LOGIN, authController.login);
-router.post(path.REGISTER, authController.register);
-router.post(path.VERIFY_TOKEN, authController.verifyToken);
-router.get(path.VERIFY_EMAIL, authController.verifyEmail);
-router.get(path.RESEND_EMAIL_VERIFICATION, verifyUser, authRestriction, authController.resendEmailVerification);
-router.put(path.CHANGE_PASSWORD, verifyUser, authRestriction, authController.changePassword);
-router.put(path.RESET_PASSWORD, authController.resetPassword);
-router.get(path.GITHUB_OAUTH, authController.githubOauth);
-router.get(path.GOOGLE_OAUTH, authController.googleOauth);
-router.get(path.FACEBOOK_OAUTH, authController.facebookOauth);
+router[path.LOGIN.method](path.LOGIN.route, authController.login);
+
+router[path.REGISTER.method](path.REGISTER.route, authController.register);
+
+router[path.VERIFY_TOKEN.method](path.VERIFY_TOKEN.route, authController.verifyToken);
+
+router[path.VERIFY_EMAIL.method](path.VERIFY_EMAIL.route, authController.verifyEmail);
+
+router[path.RESEND_EMAIL_VERIFICATION.method](path.RESEND_EMAIL_VERIFICATION.route, verifyUser, authRestriction, authController.resendEmailVerification);
+
+router[path.CHANGE_PASSWORD.method](path.CHANGE_PASSWORD.route, verifyUser, authRestriction, authController.changePassword);
+
+router[path.RESET_PASSWORD.method](path.RESET_PASSWORD.route, authController.resetPassword);
+
+router[path.GITHUB_OAUTH.method](path.GITHUB_OAUTH.route, authController.githubOauth);
+
+router[path.GOOGLE_OAUTH.method](path.GOOGLE_OAUTH.route, authController.googleOauth);
+
+router[path.FACEBOOK_OAUTH.method](path.FACEBOOK_OAUTH.route, authController.facebookOauth);
 
 app.use(cacheMiddleware)
 
-router.get(path.GET_USERS, userController.list);
-router.get(path.ME, verifyUser, userController.me);
-router.get(path.GET_USER, userController.get);
-router.put(path.SET_NEW_USER_PREFERENCE_SELF, verifyUser, userController.setPreference);
-router.put(path.UPDATE_USER_SELF, verifyUser, userController.update);
-router.delete(path.DELETE_ACCOUNT_SELF, verifyUser, userController.delete);
+router[path.GET_USERS.method](path.GET_USERS.route, userController.list);
 
-router.get(path.GET_POSTS, postController.list);
-router.get(path.GET_POST, postController.get);
-router.get(path.GET_RELATED_POSTS, verifyUser, postController.related);
-router.post(path.CREATE_POST_SELF, verifyUser, postController.create);
-router.put(path.UPDATE_POST, verifyUser, postController.update);
-router.delete(path.DELETE_POST, verifyUser, postController.delete);
-router.put(path.INCREMENT_VIEWS, postController.increment);
+router[path.ME.method](path.ME.route, verifyUser, userController.me);
 
-router.get(path.GET_RANDOM_POSTS_FOR_FEEDS, feedController.random);
-router.get(path.GET_RELEVANT_POSTS_FOR_FEEDS, verifyUser, feedController.relevant);
-router.get(path.GET_LATEST_POSTS_FOR_FEEDS, feedController.latest);
-router.get(path.GET_TOP_POSTS_FOR_FEEDS, feedController.top);
+router[path.GET_USER.method](path.GET_USER.route, userController.get);
 
-router.get(path.GET_CATEGORIES, categoryController.list);
-router.get(path.GET_POPULAR_CATEGORIES, categoryController.popular);
-router.get(path.GET_TAGS, postController.tags);
+router[path.SET_NEW_USER_PREFERENCE_SELF.method](path.SET_NEW_USER_PREFERENCE_SELF.route, verifyUser, userController.setPreference);
 
-router.get(path.GET_LIKES_COUNT_IN_POST, likeController.count)
-router.get(path.GET_LIKED_POSTS_BY_USER_SELF, likeController.listLikedPosts)
-router.post(path.LIKE_POST_SELF, verifyUser, likeController.create)
-router.delete(path.UNLIKE_POST, verifyUser, likeController.delete)
+router[path.UPDATE_USER_SELF.method](path.UPDATE_USER_SELF.route, verifyUser, userController.update);
 
-router.get(path.GET_COMMENTS_IN_POSTS, commentController.list)
-router.post(path.CREATE_COMMENT_SELF, verifyUser, commentController.create)
-router.put(path.UPDATE_COMMENT, verifyUser, commentController.update)
-router.delete(path.DELETE_COMMENT, verifyUser, commentController.delete)
+router[path.DELETE_ACCOUNT_SELF.method](path.DELETE_ACCOUNT_SELF.route, verifyUser, userController.delete);
 
-router.get(path.COUNT_FOLLOWS, followController.count)
-router.get(path.GET_FOLLOWERS, followController.listFollowers)
-router.get(path.GET_FOLLOWINGS, followController.listFollowings)
-router.post(path.FOLLOW_USER_SELF, verifyUser, followController.create)
-router.delete(path.UNFOLLOW_USER_SELF, verifyUser, followController.delete)
+router[path.GET_POSTS.method](path.GET_POSTS.route, postController.list);
 
-router.get(path.GET_SHARED_POSTS, shareController.list)
-router.post(path.SHARE_POST, verifyUser, shareController.create)
-router.put(path.UPDATE_SHARED_POST, verifyUser, shareController.update)
-router.delete(path.DELETE_SHARED_POST, verifyUser, shareController.delete)
+router[path.GET_POST.method](path.GET_POST.route, postController.get);
 
-// router.get(path.GET_NOTIFICATIONS_SELF, verifyUser)
+router[path.GET_RELATED_POSTS.method](path.GET_RELATED_POSTS.route, verifyUser, postController.related);
 
-router.get(path.GET_SUBSCRIPTION, subscriptionController.get)
-router.post(path.CREATE_SUBSCRIPTION_SELF, verifyUser, subscriptionController.create)
-router.delete(path.DELETE_SUBSCRIPTION, verifyUser, subscriptionController.delete)
+router[path.CREATE_POST_SELF.method](path.CREATE_POST_SELF.route, verifyUser, postController.create);
+
+router[path.UPDATE_POST.method](path.UPDATE_POST.route, verifyUser, postController.update);
+
+router[path.DELETE_POST.method](path.DELETE_POST.route, verifyUser, postController.delete);
+
+router[path.INCREMENT_VIEWS.method](path.INCREMENT_VIEWS.route, postController.increment);
+
+router[path.GET_RANDOM_POSTS_FOR_FEEDS.method](path.GET_RANDOM_POSTS_FOR_FEEDS.route, feedController.random);
+
+router[path.GET_RELEVANT_POSTS_FOR_FEEDS.method](path.GET_RELEVANT_POSTS_FOR_FEEDS.route, verifyUser, feedController.relevant);
+
+router[path.GET_LATEST_POSTS_FOR_FEEDS.method](path.GET_LATEST_POSTS_FOR_FEEDS.route, feedController.latest);
+
+router[path.GET_TOP_POSTS_FOR_FEEDS.method](path.GET_TOP_POSTS_FOR_FEEDS.route, feedController.top);
+
+router[path.GET_CATEGORIES.method](path.GET_CATEGORIES.route, categoryController.list);
+
+router[path.GET_POPULAR_CATEGORIES.method](path.GET_POPULAR_CATEGORIES.route, categoryController.popular);
+
+router[path.GET_TAGS.method](path.GET_TAGS.route, postController.tags);
+
+router[path.GET_LIKES_COUNT_IN_POST.method](path.GET_LIKES_COUNT_IN_POST.route, likeController.count)
+
+router[path.GET_LIKED_POSTS_BY_USER_SELF.method](path.GET_LIKED_POSTS_BY_USER_SELF.route, likeController.listLikedPosts)
+
+router[path.LIKE_POST_SELF.method](path.LIKE_POST_SELF.route, verifyUser, likeController.create)
+
+router[path.UNLIKE_POST.method](path.UNLIKE_POST.route, verifyUser, likeController.delete)
+
+router[path.GET_COMMENTS_IN_POSTS.method](path.GET_COMMENTS_IN_POSTS.route, commentController.list)
+
+router[path.CREATE_COMMENT_SELF.method](path.CREATE_COMMENT_SELF.route, verifyUser, commentController.create)
+
+router[path.UPDATE_COMMENT.method](path.UPDATE_COMMENT.route, verifyUser, commentController.update)
+
+router[path.DELETE_COMMENT.method](path.DELETE_COMMENT.route, verifyUser, commentController.delete)
+
+router[path.COUNT_FOLLOWS.method](path.COUNT_FOLLOWS.route, followController.count)
+
+router[path.GET_FOLLOWERS.method](path.GET_FOLLOWERS.route, followController.listFollowers)
+
+router[path.GET_FOLLOWINGS.method](path.GET_FOLLOWINGS.route, followController.listFollowings)
+
+router[path.FOLLOW_USER_SELF.method](path.FOLLOW_USER_SELF.route, verifyUser, followController.create)
+
+router[path.UNFOLLOW_USER_SELF.method](path.UNFOLLOW_USER_SELF.route, verifyUser, followController.delete)
+
+router[path.GET_SHARED_POSTS.method](path.GET_SHARED_POSTS.route, shareController.list)
+
+router[path.SHARE_POST.method](path.SHARE_POST.route, verifyUser, shareController.create)
+
+router[path.UPDATE_SHARED_POST.method](path.UPDATE_SHARED_POST.route, verifyUser, shareController.update)
+
+router[path.DELETE_SHARED_POST.method](path.DELETE_SHARED_POST.route, verifyUser, shareController.delete)
+
+// router[path.GET_NOTIFICATIONS_SELF.method](path.GET_NOTIFICATIONS_SELF.route, verifyUser)
+
+router[path.GET_SUBSCRIPTION.method](path.GET_SUBSCRIPTION.route, subscriptionController.get)
+
+router[path.CREATE_SUBSCRIPTION_SELF.method](path.CREATE_SUBSCRIPTION_SELF.route, verifyUser, subscriptionController.create)
+
+router[path.DELETE_SUBSCRIPTION.method](path.DELETE_SUBSCRIPTION.route, verifyUser, subscriptionController.delete)
+
 
 export default router
